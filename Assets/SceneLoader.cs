dfad6307c4698600c8cdaa2229fa7a21bc498637
yurananyodk <yuranan.yodk@bumail.net>
2025-03-29 16:55:31 +0700

    end project

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene("credit"); // ตรวจสอบให้ชื่อ Scene ตรงกัน
+    }
+}
